f56bcdd561f6666c3952a26bcfe8aa85d82effbc
Рыжов Иосиф Александрович <zerocppscript@icloud.com>
2026-03-12 22:33:42 +0300

    Второй коммит

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages friendly: use HashRouter in app.
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
+  base: '/meal-menu-planner/',
 })
